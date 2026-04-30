ea726bc0a0f351dc5ffda088a602d24c89b4e822
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-04-30 20:02:36 +0600

    added the final pricing card and add style

--- src/components/PricingInfo/Subscription/SubscriptionPrice.jsx
+++ src/components/PricingInfo/Subscription/SubscriptionPrice.jsx
@@ -1,22 +1,343 @@
-import PricingCart from "./PricingCart/PricingCart";
-
-const pricingCartData = fetch("/pricing.json").then((res) => res.json());
-
 const SubscriptionPrice = () => {
   return (
-    <div>
-      <div className="text-center pt-30 space-y-4">
+    <div className="mb-15">
+      <div className="text-center mt-15 space-y-4">
         <h1 className="text-6xl font-bold">Simple, Transparent Pricing</h1>
         <p className="text-gray-500 font-light">
           Choose the plan that fits your needs. Upgrade or downgrade anytime.
         </p>
       </div>
-      {/* Pricing section */}
-      <div>
-        <PricingCart
-          key={pricingCartData.id}
-          pricingCartData={pricingCartData}
-        ></PricingCart>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 container mx-auto mt-20">
+        {/* first card */}
+        <div className="card flex w-96 bg-base-100 shadow-sm">
+          <div className="card-body flex-1">
+            <div className="">
+              <h2 className="text-3xl font-bold">Starter</h2>
+              <p>Perfect for getting started</p>
+              <span className="text-xl">$0/month</span>
+            </div>
+            <ul className="mt-6 flex flex-col gap-2 text-xs">
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Access to 10 free tools</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Basic templates</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Community support</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>1 project per month</span>
+              </li>
+            </ul>
+            <div className="mt-6">
+              <button className="btn bg-linear-to-l text-white from-[#9514FA] to-[#4F39F6] rounded-3xl w-full">
+                Get Started Free
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* second card */}
+        <div className="card w-96 ">
+          <div className="card-body relative space-y-6 rounded-3xl bg-linear-to-l from-[#9514FA] to-[#4F39F6] text-white">
+            <div className="absolute -top-4 right-35 flex justify-center p-2 rounded-3xl border border-[#FEF3C6] bg-[#FEF3C6]">
+                <p className="text-center text-[#BB4D00]">Most Popular</p>
+            </div>
+            <div className="">
+              <h2 className="text-3xl font-bold">Pro</h2>
+              <p>Best for professionals</p>
+              <span className="text-xl">$29/month</span>
+            </div>
+            <ul className="mt-6 flex flex-col gap-2 text-xs">
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Access to all premium tools</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Unlimited templates</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Priority support</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Unlimited projects</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Cloud sync</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Advanced analytics</span>
+              </li>
+            </ul>
+            <div className="mt-6 bg-white rounded-3xl">
+              <button className="btn rounded-3xl w-full bg-linear-to-l from-[#9514FA] to-[#4F39F6] bg-clip-text text-transparent bg-white">
+                Start Pro Trial
+              </button>
+            </div>
+          </div>
+        </div>
+
+        {/* third card */}
+        <div className="card w-96 bg-base-100 shadow-sm">
+          <div className="card-body">
+            <div className="">
+              <h2 className="text-3xl font-bold">Enterprise</h2>
+              <p>For teams and businesses</p>
+              <span className="text-xl">$99/month</span>
+            </div>
+            <ul className="mt-6 flex flex-col gap-2 text-xs">
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Access to 10 free tools</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Basic templates</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Community support</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>1 project per month</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>SLA guarantee</span>
+              </li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-4 me-2 inline-block text-success"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span>Custom branding</span>
+              </li>
+              
+          
+            </ul>
+            <div className="mt-6">
+              <button className="btn bg-linear-to-l text-white from-[#9514FA] to-[#4F39F6] rounded-3xl w-full">
+                Contact Sales
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
